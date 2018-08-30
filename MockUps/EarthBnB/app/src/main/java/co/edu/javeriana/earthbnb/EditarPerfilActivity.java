20983318f308d02b196f44f05102a34b32e87ef5
Jesúspinarte <jesuspinarte@gmail.com>
2018-08-30 15:51:02 -0500

    Correction

--- MockUps/EarthBnB/app/src/main/java/co/edu/javeriana/earthbnb/EditarPerfilActivity.java
+++ MockUps/EarthBnB/app/src/main/java/co/edu/javeriana/earthbnb/EditarPerfilActivity.java
@@ -1,11 +1,7 @@
 package co.edu.javeriana.earthbnb;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class EditarPerfilActivity extends AppCompatActivity {
 
@@ -13,17 +9,5 @@ public class EditarPerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
--- MockUps/EarthBnB/app/src/main/java/co/edu/javeriana/earthbnb/Reservas.java
+++ MockUps/EarthBnB/app/src/main/java/co/edu/javeriana/earthbnb/Reservas.java
@@ -8,6 +8,6 @@ public class Reservas extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reserva);
+        setContentView(R.layout.activity_reservas);
     }
 }
