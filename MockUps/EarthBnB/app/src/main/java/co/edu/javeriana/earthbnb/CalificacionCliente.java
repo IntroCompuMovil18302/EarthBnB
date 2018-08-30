3bb3f53eb829590090009b301074065d7478fac5
MauricioVargasB <41761276+MauricioVargasB@users.noreply.github.com>
2018-08-30 14:46:31 -0500

    Add files via upload

---
+++ MockUps/EarthBnB/app/src/main/java/co/edu/javeriana/earthbnb/CalificacionCliente.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.earthbnb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalificacionCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificacion_cliente);
+    }
+}
